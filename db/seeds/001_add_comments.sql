e2418e63fb4f5f3a058d207fe8fd4518ba015046
Amit Feldman <amit@feldman.cl>
2016-10-22 12:15:13 -0400

    add migration and seed files

---
+++ db/seeds/001_add_comments.sql
@@ -0,0 +1,13 @@
+TRUNCATE TABLE comments;
+
+INSERT INTO comments (author, body)
+VALUES (
+  'Amit Feldman',
+  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+);
+
+INSERT INTO comments (author, body)
+VALUES (
+  'Ignacio García',
+  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+);
